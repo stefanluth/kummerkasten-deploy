8daf487e8e0d71f861f696ac7bfdf652b4985e72
stefanluth <stefan.luthardt@outlook.com>
2023-09-02 12:37:59 +0200

    rename deploy -> entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
 FROM alpine/k8s:1.25.13
 RUN apk add --no-cache python3 py3-pip
 RUN pip3 install --upgrade pip
+
 WORKDIR /deploy
 COPY words.txt .
 COPY generate_passphrase.py .
 COPY send_telegram.py .
-COPY deploy.sh .
 
-CMD ["/bin/sh", "-c"]
+COPY entrypoint.sh .
+RUN chmod +x entrypoint.sh
+
+ENTRYPOINT ["entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-echo "Generating passwords..."
-
 UNLOCK_PASSWORD=$(python3 generate_passphrase.py 3)
 DELETE_PASSWORD=$(python3 generate_passphrase.py 6)
 
-echo "Sending Telegram message..."
+echo $UNLOCK_PASSWORD >/shared/unlock.txt
+echo $DELETE_PASSWORD >/shared/delete.txt
 
-source ./.env
 python3 send_telegram.py \
   --token $TELEGRAM_BOT_TOKEN \
   --user $TELEGRAM_USER_ID \
   --message "Unlock password: $UNLOCK_PASSWORD\nDelete password: $DELETE_PASSWORD"
 
-echo "Starting container..."
-
-docker run \
-  -p '3000:3000' \
-  -e UNLOCK_PASSWORD=$UNLOCK_PASSWORD \
-  -e DELETE_PASSWORD=$DELETE_PASSWORD \
-  ghcr.io/stefanluth/kummerkasten:latest
+exec "$@"
